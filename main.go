bd3b4e3882a99d554cbb8f315de5003491caacda
Isaac Naylor <i.acnaylor@gmail.com>
2025-03-09 10:41:55 +0000

    Began re-write

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/wraith29/apollo/cmd"
-)
-
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Printf("%+v\n", err)
-		os.Exit(1)
-	}
+	
 }
